c38027f811a3ea37c131070005f40330c132d253
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-11-03 04:08:00 -0300

    att

--- src/features/ferias/ferias.service.js
+++ src/features/ferias/ferias.service.js
@@ -4,7 +4,7 @@ const { Op, fn, col, literal } = require('sequelize');
 const axios = require('axios');
 const { Ferias, Funcionario, Planejamento, Afastamento, Substituto, sequelize } = require('../../models');
 // SUBSTITUA PELA LINHA CORRIGIDA ABAIXO:
-const { addYears, addMonths, addDays, differenceInDays, isWithinInterval, getDay, format, getYear, parseISO, startOfDay, isValid, getMonth, setMonth, startOfYear, endOfYear } = require('date-fns');
+const { addYears, addMonths, addDays, differenceInDays, isWithinInterval, getDay, format, getYear, parseISO, startOfDay, isValid, getMonth, setMonth, startOfYear, endOfYear, endOfDay } = require('date-fns');
 /**
  * Busca os feriados nacionais de um determinado ano usando a BrasilAPI.
  */
